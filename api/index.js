4e561f6408fad703fa041286e1f350c39310f893
Oskar <aplotec.oj@gmail.com>
2026-03-23 16:08:51 +0100

    Fix serverless handler hang on Vercel

--- api/index.js
+++ api/index.js
@@ -2,14 +2,8 @@ const serverlessHttp = require('serverless-http');
 const app = require('../server/src/app');
 
 // Vercel serverless handler que delega en Express.
-// Nota: con `rewrites` Vercel puede cambiar `req.url`. Forzamos a que Express vea
-// la URL original para que los `app.use('/api/v1/tasks', ...)` casen bien.
-const handler = serverlessHttp(app);
-
-module.exports = (req, res) => {
-  if (req?.originalUrl) {
-    req.url = req.originalUrl;
-  }
-  return handler(req, res);
-};
+// `callbackWaitsForEmptyEventLoop: false` ayuda a evitar hangs/timeouts en serverless.
+module.exports = serverlessHttp(app, {
+  callbackWaitsForEmptyEventLoop: false
+});
 
